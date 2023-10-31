1a1b28e124aecb6bde2e60164d75dcae2eb68899
JACK LIN <cvse00566@gmail.com>
2023-10-31 22:28:12 +0800

    建立後臺選單 中文化

--- resources/views/admin/dashboard/environment.blade.php
+++ resources/views/admin/dashboard/environment.blade.php
@@ -1,11 +1,10 @@
 <div class="box box-default">
     <div class="box-header with-border">
-        <h3 class="box-title">Environment</h3>
+        <h3 class="box-title">{{__('Environment')}}</h3>
 
         <div class="box-tools pull-right">
             <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
             </button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
         </div>
     </div>
 
--- resources/views/admin/dashboard/title.blade.php
+++ resources/views/admin/dashboard/title.blade.php
@@ -26,10 +26,9 @@
 </style>
 
 <div class="title">
-    Laravel-admin
+    {{__('dashboard')}}
 </div>
 <div class="links">
     <a href="https://github.com/z-song/laravel-admin" target="_blank">Github</a>
     <a href="http://laravel-admin.org/docs"  target="_blank">Documentation</a>
-    <a href="http://laravel-admin.org/demo"  target="_blank">Demo</a>
 </div>
